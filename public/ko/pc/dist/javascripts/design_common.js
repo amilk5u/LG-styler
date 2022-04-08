990d5a57244c0876051500f3ddf653bd42879aed
amilk5u <amilk5u@naver.com>
2022-04-08 23:20:54 +0900

    first commit

--- public/ko/pc/dist/javascripts/design_common.js
+++ public/ko/pc/dist/javascripts/design_common.js
@@ -35,5 +35,21 @@ $window.load(function () {
 function layout() {
 }
 function main() {
+   var careTips = new Swiper(".care_tips", {
+      slidesPerView: 5,
+      spaceBetween: 24,
+   });
+   var lineupSlide = new Swiper(".lineup_slide", {
+      slidesPerView: 4.5,
+      // slidesPerView: "auto",
+      spaceBetween: 20,
+      // centeredSlides: true,
+      scrollbar: {
+         el: '.swiper-scrollbar',
+         draggable: true,
+      },
+   });
+
+
 
 }

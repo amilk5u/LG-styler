dabd9b02823701e3e284aec6207d18f8440ae9c6
amilk5u <amilk5u@naver.com>
2022-04-15 14:52:15 +0900

    22.04.15 first commit

---
+++ public/_backup/220415/ko_v1.0 - 별수정전까지 최종완료/pc/dist/javascripts/design_common.js
@@ -0,0 +1,219 @@
+"use strict";
+var winW;
+var winH;
+var esStep = "Expo.ease";
+var esOut = "Expo.easeOut";
+var $window = $(window);
+var winSc = $(window).scrollTop();
+var $header = $("#header");
+var $html = $("html");
+var navAni;
+var controller = null;
+//gnb
+var $gnb = $("#gnb"),
+   $navBtn = $gnb.find("button");
+
+//배열 순차 더한 값
+var triggerScrVar = [];
+
+$window.load(function () {
+   var _this = $(this);
+   winW = _this.width();
+   winH = _this.height();
+   winSc = _this.scrollTop();
+   $window.on("resize", function () {
+      winW = _this.width();
+      winH = _this.height();
+   });
+   _this.trigger("resize");
+   $(window).scroll(function () {
+      winSc = _this.scrollTop();
+   });
+   layout();
+   main();
+});
+function layout() {
+   
+}
+function main() {
+   const $header = $("header"),
+      $topNav = $(".top_nav"),
+      $visual = $("#visual"),
+      $ClothingCareTips = $("#ClothingCareTips"),
+      $AboutStyler = $("#AboutStyler"),
+      $FAQ = $("#FAQ"),
+      $StylerLineup = $("#StylerLineup");
+   let careTips, yOffset,
+      secWrap = [],
+      currentScene = 0;
+   let $topNavButton = $topNav.find("li");
+   let topNavHeight = $topNav.outerHeight();
+
+   $(".sec-wrap").each(function () {
+      secWrap.push($(this));
+   })
+
+   function careTipsSlide() {
+      // 윈도우 넓이가 768 미만이고, careTip가 undefined 가 아닐때
+      const winWidth = window.innerWidth;
+      if (winWidth < 768 && careTips !== undefined) {
+         careTips.destroy(); // 중지
+         careTips = undefined;
+         // 윈도우가 768 이상이고, careTips 가 undefined 일때
+      } else if (winWidth > 768 && careTips === undefined) {
+         careTips = new Swiper(".care_tips", {
+            slidesPerView: 2,
+            spaceBetween: 24,
+            breakpoints: {
+               769: {
+                  slidesPerView: 3,  //브라우저가 768보다 클 때
+                  spaceBetween: 24,
+               },
+               1025: {
+                  slidesPerView: 4,  //브라우저가 1024보다 클 때
+                  spaceBetween: 24,
+               },
+               1601: {
+                  slidesPerView: 5,  //브라우저가 1024보다 클 때
+                  spaceBetween: 24,
+               },
+            },
+         });
+      }
+   }
+
+
+   const lineupSlide = new Swiper(".lineup_slide", {
+      slidesPerView: 1.6,
+      spaceBetween: 20,
+      scrollbar: {
+         el: ".swiper-scrollbar",
+         draggable: true,
+      },
+      breakpoints: {
+         769: {
+            slidesPerView: 2.6,
+            spaceBetween: 20,
+         },
+         1025: {
+            slidesPerView: 3.6,
+            spaceBetween: 20,
+         },
+         1201: {
+            slidesPerView: 4.6,
+            spaceBetween: 20,
+         },
+      },
+   });
+
+   const articleLine = new Swiper(".article_line", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      scrollbar: {
+         el: ".swiper-scrollbar",
+         draggable: true,
+      },
+      breakpoints: {
+         769: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+         },
+         1025: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+         },
+         1201: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+         },
+      },
+   });
+
+   function scrollNav() {
+      let prevHeight = $visual.outerHeight() + $header.outerHeight();
+      let prevSecHeight = 0;
+      // 이전 높이 보다 컸을 때 add 작을 때 remove 해라
+      winSc > prevHeight ? $topNav.addClass("on") : $topNav.removeClass("on")
+
+      if (yOffset > prevHeight) {
+         for (let i = 0; i < currentScene; i++) {
+            prevSecHeight += secWrap[i].outerHeight();
+         }
+         if (yOffset > prevSecHeight + prevHeight + secWrap[currentScene].outerHeight() - topNavHeight) {
+            currentScene++;
+         }
+         if (yOffset < prevSecHeight + prevHeight - topNavHeight) {
+            if (currentScene === 0) return;
+            currentScene--;
+         }
+         $topNav.find("li").find("button").removeClass("active")
+         $topNav.find("li").eq(currentScene).find("button").addClass("active")
+      } else {
+         $topNav.find("li").find("button").removeClass("active")
+      }
+   }
+
+   // 네비게이션 클릭
+   $topNavButton.on("click", function () {
+      let _this = $(this),
+         _index = _this.index();
+      $topNav.find("li button").removeClass("active");
+      if (_index !== 0) {
+         TweenMax.to($("html, body"), .5,{ scrollTop:secWrap[_index].offset().top + 5 - topNavHeight });
+      } else {
+         // window.scrollTo({ top: secWrap[0].offset().top + 3, behavior: "smooth" });
+         TweenMax.to($("html, body"), .5,{ scrollTop:secWrap[_index].offset().top + 5});
+      }
+   });
+
+
+   // Faq 질문 답변 클릭
+   function faqAction() {
+      const $questDt = $FAQ.find("dt");
+      $questDt.on("click", function () {
+         let _this = $(this);
+         let $questionDl = _this.parent(),
+            $questionDd = $questionDl.find("dd"),
+            $answerP = $questionDl.find(".answer").outerHeight();
+
+         if (!$questionDl.hasClass("on")) {
+            TweenMax.to($questionDd, .25, { height: $answerP })
+            $questionDl.addClass("on");
+         } else {
+            TweenMax.to($questionDd, .25, { height: 0 })
+            $questionDl.removeClass("on");
+         }
+      })
+   }
+
+   // 비디오 더보기 more_add 
+   const $sbVideo = $AboutStyler.find(".sb_video"),
+      $moreVideoBtn = $AboutStyler.find(".more_video_btn button");
+
+   $moreVideoBtn.on("click", function () {
+      let $moreVideoBtnH = $moreVideoBtn.outerHeight();
+      let $sbVideoH = $sbVideo.find("li").outerHeight();
+      if (!$sbVideo.hasClass("more_add")) {
+         $sbVideo.addClass("more_add");
+         $sbVideo.height($moreVideoBtnH + $sbVideoH);
+      } else {
+         $sbVideo.removeClass("more_add");
+         $sbVideo.height("inherit");
+      }
+   });
+
+   $(window).on("resize", function () {
+      careTipsSlide();
+   });
+   window.addEventListener("scroll", function () {
+      yOffset = window.pageYOffset;
+      $(".fix").text(winSc);
+      scrollNav();
+   });
+
+   function init() {
+      careTipsSlide();
+      faqAction();
+   }
+   init();
+}
--- public/ko/pc/dist/javascripts/design_common.js
+++ public/ko/pc/dist/javascripts/design_common.js
@@ -36,184 +36,203 @@ function layout() {
    
 }
 function main() {
-   const $header = $("header"),
-      $topNav = $(".top_nav"),
-      $visual = $("#visual"),
-      $ClothingCareTips = $("#ClothingCareTips"),
-      $AboutStyler = $("#AboutStyler"),
-      $FAQ = $("#FAQ"),
-      $StylerLineup = $("#StylerLineup");
-   let careTips, yOffset,
-      secWrap = [],
-      currentScene = 0;
-   let $topNavButton = $topNav.find("li");
-   let topNavHeight = $topNav.outerHeight();
+   const $header = $("header");
+   let yOffset;
+   
+   // 링크 차단
+   $(".no_open").find("a").on("click", function (e) {
+      e.preventDefault();
+   });
 
-   $(".sec-wrap").each(function () {
-      secWrap.push($(this));
-   })
+   function microSite() {
+      let careTips;
+      let secWrap = [];
+      let currentScene = 0;
+      const $topNav = $(".top_nav"),
+         $visual = $("#visual"),
+         $ClothingCareTips = $("#ClothingCareTips"),
+         $AboutStyler = $("#AboutStyler"),
+         $FAQ = $("#FAQ"),
+         $StylerLineup = $("#StylerLineup");
+      let $topNavButton = $topNav.find("li");
+      let topNavHeight = $topNav.outerHeight();
 
-   function careTipsSlide() {
-      // 윈도우 넓이가 768 미만이고, careTip가 undefined 가 아닐때
-      const winWidth = window.innerWidth;
-      if (winWidth < 768 && careTips !== undefined) {
-         careTips.destroy(); // 중지
-         careTips = undefined;
-         // 윈도우가 768 이상이고, careTips 가 undefined 일때
-      } else if (winWidth > 768 && careTips === undefined) {
-         careTips = new Swiper(".care_tips", {
-            slidesPerView: 2,
-            spaceBetween: 24,
-            breakpoints: {
-               769: {
-                  slidesPerView: 3,  //브라우저가 768보다 클 때
-                  spaceBetween: 24,
+      // section wrap 에 담기
+      $(".sec-wrap").each(function () {
+         secWrap.push($(this));
+      });
+
+      function careTipsSlide() {
+         // 윈도우 넓이가 768 미만이고, careTip가 undefined 가 아닐때
+         const winWidth = window.innerWidth;
+         if (winWidth < 768 && careTips !== undefined) {
+            careTips.destroy(); // 중지
+            careTips = undefined;
+            // 윈도우가 768 이상이고, careTips 가 undefined 일때
+         } else if (winWidth > 768 && careTips === undefined) {
+            careTips = new Swiper(".care_tips", {
+               slidesPerView: 2,
+               spaceBetween: 24,
+               breakpoints: {
+                  769: {
+                     slidesPerView: 3,  //브라우저가 769 보다 클 때
+                     spaceBetween: 24,
+                  },
+                  1025: {
+                     slidesPerView: 4,  //브라우저가 1024보다 클 때
+                     spaceBetween: 24,
+                  },
+                  1201: {
+                     slidesPerView: 5,
+                     spaceBetween: 24,
+                  },
                },
-               1025: {
-                  slidesPerView: 4,  //브라우저가 1024보다 클 때
-                  spaceBetween: 24,
-               },
-               1601: {
-                  slidesPerView: 5,  //브라우저가 1024보다 클 때
-                  spaceBetween: 24,
-               },
+            });
+         }
+      }
+
+      const lineupSlide = new Swiper(".lineup_slide", {
+         slidesPerView: 1.6,
+         spaceBetween: 20,
+         scrollbar: {
+            el: ".swiper-scrollbar",
+            draggable: true,
+         },
+         breakpoints: {
+            769: {
+               slidesPerView: 2.6,
+               spaceBetween: 20,
             },
-         });
+            1025: {
+               slidesPerView: 3.6,
+               spaceBetween: 20,
+            },
+            1201: {
+               slidesPerView: 4.6,
+               spaceBetween: 20,
+            },
+         },
+      });
+
+
+      // Faq 질문 답변 클릭
+      function faqAction() {
+         const $questDt = $FAQ.find("dt");
+         $questDt.on("click", function () {
+            let _this = $(this);
+            let $questionDl = _this.parent(),
+               $questionDd = $questionDl.find("dd"),
+               $answerP = $questionDl.find(".answer").outerHeight();
+
+            if (!$questionDl.hasClass("on")) {
+               TweenMax.to($questionDd, .25, { height: $answerP });
+               $questionDl.addClass("on");
+            } else {
+               TweenMax.to($questionDd, .25, { height: 0 });
+               $questionDl.removeClass("on");
+            }
+         })
       }
-   }
 
+      // More Video 
+      const $sbVideo = $AboutStyler.find(".sb_video"),
+         $moreVideoBtn = $AboutStyler.find(".more_video_btn button");
 
-   const lineupSlide = new Swiper(".lineup_slide", {
-      slidesPerView: 1.6,
-      spaceBetween: 20,
-      scrollbar: {
-         el: ".swiper-scrollbar",
-         draggable: true,
-      },
-      breakpoints: {
-         769: {
-            slidesPerView: 2.6,
-            spaceBetween: 20,
-         },
-         1025: {
-            slidesPerView: 3.6,
-            spaceBetween: 20,
-         },
-         1201: {
-            slidesPerView: 4.6,
-            spaceBetween: 20,
-         },
-      },
-   });
-
-   const articleLine = new Swiper(".article_line", {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      scrollbar: {
-         el: ".swiper-scrollbar",
-         draggable: true,
-      },
-      breakpoints: {
-         769: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-         },
-         1025: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-         },
-         1201: {
-            slidesPerView: 4,
-            spaceBetween: 30,
-         },
-      },
-   });
-
-   function scrollNav() {
-      let prevHeight = $visual.outerHeight() + $header.outerHeight();
-      let prevSecHeight = 0;
-      // 이전 높이 보다 컸을 때 add 작을 때 remove 해라
-      winSc > prevHeight ? $topNav.addClass("on") : $topNav.removeClass("on")
-
-      if (yOffset > prevHeight) {
-         for (let i = 0; i < currentScene; i++) {
-            prevSecHeight += secWrap[i].outerHeight();
-         }
-         if (yOffset > prevSecHeight + prevHeight + secWrap[currentScene].outerHeight() - topNavHeight) {
-            currentScene++;
-         }
-         if (yOffset < prevSecHeight + prevHeight - topNavHeight) {
-            if (currentScene === 0) return;
-            currentScene--;
-         }
-         $topNav.find("li").find("button").removeClass("active")
-         $topNav.find("li").eq(currentScene).find("button").addClass("active")
-      } else {
-         $topNav.find("li").find("button").removeClass("active")
-      }
-   }
-
-   // 네비게이션 클릭
-   $topNavButton.on("click", function () {
-      let _this = $(this),
-         _index = _this.index();
-      $topNav.find("li button").removeClass("active");
-      if (_index !== 0) {
-         TweenMax.to($("html, body"), .5,{ scrollTop:secWrap[_index].offset().top + 1 - topNavHeight });
-      } else {
-         window.scrollTo({ top: secWrap[0].offset().top + 1, behavior: "smooth" });
-         TweenMax.to($("html, body"), .5,{ scrollTop:secWrap[_index].offset().top + 1});
-      }
-   });
-
-
-   // Faq 질문 답변 클릭
-   function faqAction() {
-      const $questDt = $FAQ.find("dt");
-      $questDt.on("click", function () {
-         let _this = $(this);
-         let $questionDl = _this.parent(),
-            $questionDd = $questionDl.find("dd"),
-            $answerP = $questionDl.find(".answer").outerHeight();
-
-         if (!$questionDl.hasClass("on")) {
-            TweenMax.to($questionDd, .25, { height: $answerP })
-            $questionDl.addClass("on");
+      $moreVideoBtn.on("click", function () {
+         let $moreVideoBtnH = $moreVideoBtn.outerHeight();
+         let $sbVideoH = $sbVideo.find("li").outerHeight();
+         if (!$sbVideo.hasClass("more_add")) {
+            $sbVideo.addClass("more_add");
+            $sbVideo.height($moreVideoBtnH + $sbVideoH);
          } else {
-            TweenMax.to($questionDd, .25, { height: 0 })
-            $questionDl.removeClass("on");
+            $sbVideo.removeClass("more_add");
+            $sbVideo.height("inherit");
          }
-      })
-   }
+      });
 
-   // 비디오 더보기 more_add 
-   const $sbVideo = $AboutStyler.find(".sb_video"),
-      $moreVideoBtn = $AboutStyler.find(".more_video_btn button");
 
-   $moreVideoBtn.on("click", function () {
-      let $moreVideoBtnH = $moreVideoBtn.outerHeight();
-      let $sbVideoH = $sbVideo.find("li").outerHeight();
-      if (!$sbVideo.hasClass("more_add")) {
-         $sbVideo.addClass("more_add");
-         $sbVideo.height($moreVideoBtnH + $sbVideoH);
-      } else {
-         $sbVideo.removeClass("more_add");
-         $sbVideo.height("inherit");
+      // Navigation
+      function scrollNav() {
+         let prevHeight = $visual.outerHeight() + $header.outerHeight();
+         let prevSecHeight = 0;
+         // 이전 높이 보다 컸을 때 add 작을 때 remove 해라
+         yOffset > prevHeight ? $topNav.addClass("on") : $topNav.removeClass("on");
+
+         if (yOffset > prevHeight) {
+            for (let i = 0; i < currentScene; i++) {
+               prevSecHeight += secWrap[i].outerHeight();
+            }
+            // 현재 스크롤 값이 이전높이값과 현재 높이값을 더한것과 네비를 뺀값보다 컸을 때 
+            if (yOffset > prevSecHeight + prevHeight + secWrap[currentScene].outerHeight() - topNavHeight) {
+               currentScene++;
+            }
+            // 현재 스크롤 값이 이전높이값에서 네비를 뺀값보다 작았을 때
+            if (yOffset < prevSecHeight + prevHeight - topNavHeight) {
+               currentScene--;
+            }
+            $topNav.find("li").find("button").removeClass("active");
+            $topNav.find("li").eq(currentScene).find("button").addClass("active");
+         } else {
+            $topNav.find("li").find("button").removeClass("active");
+         }
       }
-   });
 
-   $(window).on("resize", function () {
-      careTipsSlide();
-   });
-   window.addEventListener("scroll", function () {
-      yOffset = window.pageYOffset;
-      $(".fix").text(winSc);
-      scrollNav();
-   });
+      // Navigation Click
+      $topNavButton.on("click", function () {
+         let _this = $(this),
+            _index = _this.index();
+         $topNav.find("li button").removeClass("active");
+         if (_index !== 0) {
+            TweenMax.to($("html, body"), .5, { scrollTop: secWrap[_index].offset().top + 5 - topNavHeight });
+         } else {
+            // window.scrollTo({ top: secWrap[0].offset().top + 3, behavior: "smooth" });
+            TweenMax.to($("html, body"), .5, { scrollTop: secWrap[_index].offset().top + 5 });
+         }
+      });
 
-   function init() {
+      window.addEventListener("scroll", function () {
+         yOffset = window.pageYOffset;
+         $(".fix").text(yOffset);
+         scrollNav();
+      });
+      $(window).on("resize", function () {
+         careTipsSlide();
+      });
       careTipsSlide();
       faqAction();
    }
-   init();
+
+   function dryCleaning() {
+      const articleLine = new Swiper(".article_line", {
+         slidesPerView: 1,
+         spaceBetween: 30,
+         scrollbar: {
+            el: ".swiper-scrollbar",
+            draggable: true,
+         },
+         breakpoints: {
+            769: {
+               slidesPerView: 2,
+               spaceBetween: 30,
+            },
+            1025: {
+               slidesPerView: 3,
+               spaceBetween: 30,
+            },
+            1201: {
+               slidesPerView: 4,
+               spaceBetween: 30,
+            },
+         },
+      });
+   }
+
+   if ($(".container").hasClass("micro_wrap")) {
+      microSite();
+      console.log("micro_wrap");
+   }
+   if ($(".container").hasClass("dryCleaning_wrap")) {
+      dryCleaning();
+      console.log("dryCleaning_wrap");
+   }
 }
